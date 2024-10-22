9d041372f740adacc846bcd003ecc91db44a6e6e
Irina Ochoa Ramírez <irinaochoa32@gmail.com>
2024-10-22 13:04:02 -0500

    primer commit

---
+++ BackV2/src/main/java/com/tesis/BackV2/BackV2Application.java
@@ -0,0 +1,13 @@
+package com.tesis.BackV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackV2Application.class, args);
+	}
+
+}
